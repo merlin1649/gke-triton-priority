d813be229820061a31bb6144652d4cdf3223dc09
Kevin Tsai <kevin0259@gmail.com>
2021-02-10 13:30:22 -0800

    2

--- locust/locust-tier1-shape.py
+++ locust/locust-tier1-shape.py
@@ -2,59 +2,24 @@ import time
 from locust import HttpUser, task, between
 from locust import LoadTestShape
 import json
-import math
+from math import sin, pi
 
-# class CustomLoad(locust.LoadTestShape):
-#     """
-#     The load shape class that progressively adds users up to the maximum.
-#     After the maximum load has been reached the class starts to progressively decrease 
-#     the load till all users are stopped.
-#     """
-
-#     max_steps = 11
-#     step_time = 60
-#     step_load = 2
-#     spawn_rate = 10
-
-#     def tick(self):
-#         run_time = self.get_run_time()
-
-#         current_step = math.floor(run_time / self.step_time) + 1
-
-#         if current_step <= self.max_steps:
-#             return (current_step * self.step_load, self.spawn_rate)
-#         else:
-#             return None
-
-class DoubleWave(LoadTestShape):
+class SineLoad(LoadTestShape):
     """
-    A shape to immitate some specific user behaviour. In this example, midday
-    and evening meal times. First peak of users appear at time_limit/3 and
-    second peak appears at 2*time_limit/3
-    Settings:
-        min_users -- minimum users
-        peak_one_users -- users in first peak
-        peak_two_users -- users in second peak
-        time_limit -- total length of test
     """
 
-    min_users = 20
-    peak_one_users = 200
-    peak_two_users = 200
-    time_limit = 200
+    target_users   = 500
+    sine_amplitude = 5   # percent
+    sine_period    = 20  # seconds
+    run_time       = 600 # seconds
 
     def tick(self):
         run_time = round(self.get_run_time())
 
         if run_time < self.time_limit:
-            user_count = (
-                (self.peak_one_users - self.min_users)
-                * math.e ** -(((run_time / (self.time_limit / 10 * 2 / 3)) - 5) ** 2)
-                + (self.peak_two_users - self.min_users)
-                * math.e ** -(((run_time / (self.time_limit / 10 * 2 / 3)) - 10) ** 2)
-                + self.min_users
-            )
-            return (round(user_count), round(user_count))
+            theta = 2*pi*(run_time % sine_period)/sine_period
+            user_count = round(target_users*(sin(theta)*sine_amplitude/100 + 1))
+            return (user_count,user_count)
         else:
             return None
 
